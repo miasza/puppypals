5c669916406c511927561bd3d66a4de0d6cdb8a7
Stephan Lotze <stephan.lotze@outlook.com>
2019-05-20 18:39:37 +0200

    update visit model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+User.delete_all
+Dog.delete_all
+
+puts "Creating new users and dogs"
+
+
+#30 users for user model
+30.times do
+
+  email = Faker::Internet.email
+  password = Faker::Code.sin
+
+  User.create(email: email, password: password)
+
+end
+
+#50 dogs for dog model
+50.times do
+
+  name = Faker::Creature::Dog.name
+  age = rand(0..20)
+  breed = Faker::Creature::Dog.breed
+  size = Faker::Creature::Dog.size
+  price = rand(5..50)
+
+  x = Dog.new(name: name, age: age, breed: breed, size: size, price: price)
+  x.user = User.find(rand(1..30))
+  x.save
+
+end
+
+puts "Database records created"
