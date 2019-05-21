50b91b5c96a7ec99f809120e81245e382702862b
Mia Szarvas <mia.szarvas@gmail.com>
2019-05-21 11:17:44 +0200

    update routing to display visit form in dogs show

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,31 +1,19 @@
 class VisitsController < ApplicationController
-  def index
-    @user = current_user
-    @dog = Dog.find(params[:dog_id])
-    @visits = Visit.where(user_id: @user.id, dog_id: @dog.id)
-  end
-
-  def new
-    @visit = Visit.new
-    @dog = Dog.find(params[:dog_id])
-  end
-
   def create
-    @user = current_user
     @dog = Dog.find(params[:dog_id])
     @visit = Visit.new(visit_params)
-    @visit.user = @user
+    @visit.user = current_user
     @visit.dog = @dog
     if @visit.save
       redirect_to dog_visits_path(@dog)
     else
-      render :new
+      render 'dogs/show'
     end
   end
 
   def destroy
     @dog = Dog.find(params[:dog_id])
-    @visit = Visit.find(params[:id])
+    @visit = @dog.visit
     @visit.destroy
     redirect_to dog_visits_path(@dog)
   end
