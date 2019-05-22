388d5e79a179c7edfc7fbd3f01a8ad0c43197b00
Celina Gerecke <celina.gerecke@gmail.com>
2019-05-22 14:08:41 +0200

    addautocomplete form and on map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,6 +6,8 @@ const fitMapToMarkers = (map, markers) => {
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
 };
 
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -25,6 +27,7 @@ const initMapbox = () => {
         .addTo(map);
     });
     fitMapToMarkers(map, markers);
+  map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
   }
 };
 
