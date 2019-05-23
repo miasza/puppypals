b0e4f95b97960e77cf42a980cc4a297771f9ef65
Mia Szarvas <mia.szarvas@gmail.com>
2019-05-23 13:32:20 +0200

    reroute to playdates path

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2019_05_23_105556) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
