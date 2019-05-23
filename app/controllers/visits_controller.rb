89f07425d0ed8def5f3b6d38089f1be4faf68b22
Stephan Lotze <stephan.lotze@outlook.com>
2019-05-23 11:10:24 +0200

    update converted date into string

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -8,7 +8,7 @@ class VisitsController < ApplicationController
     @visit.user = current_user
     @visit.dog = @dog
     @visits = @dog.visits
-    if @visit.date < Date.today
+    if @visit.date < Date.today.to_s
       flash[:alert] = "The date entered has already passed. Please provide future date"
       render 'dogs/show'
     else
