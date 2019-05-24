de2d2ee7fda4b0430c9fae5ee9f6746e842412f9
Mia Szarvas <mia.szarvas@gmail.com>
2019-05-24 12:50:18 +0200

    design bookings card

--- app/javascript/components/_pals_display.js
+++ app/javascript/components/_pals_display.js
@@ -5,20 +5,23 @@ const items = [ pals, bookings ];
 const pals_list = document.querySelector("#pals-list");
 const bookings_list = document.querySelector("#bookings-list");
 
-items.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (event.currentTarget() == bookings) {
-    pals_list.classList.remove("active");
-    pals_list.classList.add("hidden");
-    bookings_list.classList.remove("hidden");
-    bookings_list.classList.add("active");
+items.forEach((item) => {
+  if (item !== null) {
+    item.addEventListener("click", (event) => {
+      event.preventDefault();
+      if (item === bookings) {
+        pals_list.classList.remove("active");
+        pals_list.classList.add("hidden");
+        bookings_list.classList.remove("hidden");
+        bookings_list.classList.add("active");
+      };
+      if (item === pals) {
+        bookings_list.classList.remove("active");
+        bookings_list.classList.add("hidden");
+        pals_list.classList.remove("hidden");
+        pals_list.classList.add("active");
+      };
+    });
   }
-  else if (event.currentTarget() == pals) {
-    bookings_list.classList.remove("active");
-    bookings_list.classList.add("hidden");
-    pals_list.classList.remove("hidden");
-    pals_list.classList.add("active");
-  };
-
   // Do something (callback)
 });
